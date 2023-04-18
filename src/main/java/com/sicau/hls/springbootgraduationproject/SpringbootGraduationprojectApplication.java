dc0c906278bb0d147efd5dd0a4a160d622236619
ssm <1290375385@qq.com>
2023-04-18 22:04:46 +0800

    项目创建

---
+++ src/main/java/com/sicau/hls/springbootgraduationproject/SpringbootGraduationprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sicau.hls.springbootgraduationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGraduationprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGraduationprojectApplication.class, args);
+    }
+
+}
